b4f663e7bcd483147eae07e08d2ce997108508fb
Sathish <vtsathishprasad@gmail.com>
2024-01-07 13:36:52 -0800

    u

--- app.py
+++ app.py
@@ -316,7 +316,7 @@ def amenities(url):
         options = Options()
         options.add_argument("--headless")
         options.add_argument('--disable-gpu')
-        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
+        #options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
