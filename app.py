20b8e8043be2c3c38f646b4c9e952a78b1333674
Sathish <vtsathishprasad@gmail.com>
2024-01-07 13:52:25 -0800

    u

--- app.py
+++ app.py
@@ -315,8 +315,14 @@ def amenities(url):
     try:
         options = Options()
         #options.add_argument("--headless")
-        options.add_argument("--headless=new")
-        options.add_argument('--disable-gpu')
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-features=NetworkService")
+        options.add_argument("--window-size=1920x1080")
+        options.add_argument("--disable-features=VizDisplayCompositor")
 
 
 
